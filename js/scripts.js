7cd28e87ec964eb1a13cd90096fca44a30d8100e
cleri philiastides & ian bevel <student@epicodus.com>
2015-06-01 16:11:57 -0700

    add a category with multiple tasks and notes associated with tasks

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,8 @@ $(document).ready(function() {
   $("#add-task").click(function() {
     $("#new-tasks").append('<div class="new-task">' +
                               '<div class="form-group">' +
-                                '<label for="new-description">Description</label>' +
-                                '<input type="text" class="form-control new-description">' +
+                                '<label for="new-task-name">Description</label>' +
+                                '<input type="text" class="form-control new-task-name">' +
                               '</div>' +
                               '<div class="form-group">' +
                                 '<label for="new-notes">Notes</label>' +
@@ -20,8 +20,35 @@ $(document).ready(function() {
 
     var newToDoList = { category: inputtedCategory, tasks: [] };
 
+    $(".new-task").each(function() {
+      var inputtedTaskName = $(this).find("input.new-task-name").val();
+      var inputtedNotes = $(this).find("input.new-notes").val();
+
+      var newTask = { taskName: inputtedTaskName, notes: inputtedNotes };
+      newToDoList.tasks.push(newTask);
+    });
+
 
     $("ul#categories").append("<li><span class='category'>" + newToDoList.category + " " + "</span></li>");
+
+    $(".category").last().click(function() {
+      $("#show-category").show();
+
+      $("#show-category h2").text(newToDoList.category);
+
+      $("ul#tasks").text("");
+      newToDoList.tasks.forEach(function(task) {
+        $("ul#tasks").append("<li>" + task.taskName + " -- " + task.notes + "</li>");
+      });
+    });
+
+    $("input#new-task-name").val("");
+    $("input#new-category").val("");
+    $("input#new-notes").val("");
+
+    $("#new-todolist").each(function() {
+      this.reset();
+    });
 
 
   });
